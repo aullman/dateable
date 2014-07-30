b65ca130a839d04a18faa308e77fdc1e0d100e45
Luke Eller <luke.eller@bigcommerce.com>
2014-07-30 21:50:03 +1000

    Working partials

--- app/routes/partials.js
+++ app/routes/partials.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET partials page. */
-router.get('/partials/:name', function(req, res) {
-  var name = req.params.name;
-  res.render('app/partials/' + name);
+router.get('*', function(req, res) {
+  var name = req.url.replace(/^\//, '');
+  res.render('partials/vents/index');
 });
 
 module.exports = router;
