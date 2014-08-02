72974b7de037f2cabc7a82cd763ec3bd2e02bf58
Luke Eller <luke.eller@bigcommerce.com>
2014-08-02 14:14:27 +1000

    Rest modal when closed

--- app/client/javascripts/controllers/events/index-controller.js
+++ app/client/javascripts/controllers/events/index-controller.js
@@ -1,10 +1,18 @@
 angular.module('speed-dating.controllers.events.index', ['speed-dating.services.event']).controller('EventsCtrl', ['$scope', 'EventService',
   function($scope, EventService) {
 
-    $scope.event = {
-      name: null,
-      description: null
+    var resetModal = function resetModal() {
+      $scope.event = {
+        name: null,
+        description: null,
+        creating: false
+      };
+      if ($scope.newEvent) {
+        $scope.newEvent.$setPristine();
+      }
     };
+    resetModal();
+    $scope.$on('hidden.sd.modal', resetModal);
 
     EventService.all().then(function (events) {
       $scope.events = events;
@@ -12,8 +20,9 @@ angular.module('speed-dating.controllers.events.index', ['speed-dating.services.
 
     $scope.createEvent = function createEvent(event) {
       if ($scope.newEvent.$valid) {
+        event.creating = true;
         EventService.create(event.name, event.description).then(function () {
-          $scope.dismiss();
+          $scope.dismissModal();
         });
       }
     };
@@ -27,7 +36,6 @@ angular.module('speed-dating.controllers.events.index', ['speed-dating.services.
       }
 
     };
-
 
     $scope.msg = 'Here are the speed dating events.';
   }]);
--- app/client/javascripts/directives/modal.js
+++ app/client/javascripts/directives/modal.js
@@ -1,10 +1,17 @@
 angular.module('speed-dating.directives.modal', []).directive('sdModal', function() {
   return {
     restrict: 'A',
-    link: function(scope, element, attr) {
-      scope.dismiss = function() {
+    link: function($scope, element, attr) {
+      $scope.dismissModal = function() {
         element.modal('hide');
       };
+
+      element.on('show.bs.modal shown.bs.modal hidden.bs.modal hide.bs.modal loaded.bs.modal', function (event) {
+        $scope.$emit([event.type, 'sd.modal'].join('.'));
+        if (!$scope.$$phase) {
+          $scope.$apply();
+        }
+      });
     }
   }
 });
